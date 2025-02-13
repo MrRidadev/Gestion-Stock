d8ac57b0a1638d952808aec21ad5e4e921d942b6
ridaDev <kiratterida@gmail.com>
2025-02-13 17:39:47 +0100

    ajoyter printSQLException

--- src/main/java/dao/ProduitDao.java
+++ src/main/java/dao/ProduitDao.java
@@ -148,7 +148,19 @@ public class ProduitDao {
     }
 
 	private void printSQLException(SQLException e) {
-		// TODO Auto-generated method stub
+		for (Throwable e1: e) {
+            if (e1 instanceof SQLException) {
+                e1.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e1).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e1).getErrorCode());
+                System.err.println("Message: " + e1.getMessage());
+                Throwable t = e.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
 		
 	}
     
